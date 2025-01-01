873d76ec08907098c9e179b1e7a74ee67bdb7236
timlee <hungtington.dev@gmail.com>
2025-01-02 05:21:36 +0800

    ✨ feat: add Tailwind + Shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vite from './vite.config.js';
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false, // according to https://ui.shadcn.com/docs/installation/astro
+  })],
   vite
 });
