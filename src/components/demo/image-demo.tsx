014c66299acf2e0a0d20a1faa4279ee81e3a407f
timlee <hungtington.dev@gmail.com>
2025-03-09 13:33:08 -0400

    feat: make Image great

---
+++ src/components/demo/image-demo.tsx
@@ -0,0 +1,19 @@
+import { Image } from "@/components/ui/Image/Image"
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import smallerSizeDarkImage from "@/assets/demo/image-demo--dark.webp"
+import originalSizeDarkImage from "@/assets/demo/image-demo--dark.jpg"
+import smallerSizeLightImage from "@/assets/demo/image-demo--light.webp"
+import originalSizeLightImage from "@/assets/demo/image-demo--light.jpg"
+import { useColorScheme } from '@/hooks/useColorScheme'
+
+export default () => {
+  const { isDark } = useColorScheme();
+  return (
+    <Image
+      effect="blur"
+      className="tw:w-full tw:h-full"
+      src={isDark ? originalSizeDarkImage.src : originalSizeLightImage.src}
+      placeholderSrc={isDark ? smallerSizeDarkImage.src : smallerSizeLightImage.src}
+    />
+  );
+}
