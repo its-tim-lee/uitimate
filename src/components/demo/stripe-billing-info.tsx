14bd2f5f9c5d021a0e953d6c27c860a8669acb11
timlee <hungtington.dev@gmail.com>
2025-03-18 06:33:33 -0400

    feat: add BillingInfo

--- src/components/demo/stripe-billing-info.tsx
+++ src/components/demo/stripe-billing-info.tsx
@@ -9,22 +9,30 @@ import { z } from "zod"
 import { Form, FormControl, FormItem, FormLabel, FormMessage } from "../ui/Form/Form"
 import { Input } from "../ui/Input/Input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/Select/Select"
+import { PopoverContent, PopoverTrigger } from "../ui/Popover/Popover"
+import { Popover } from "../ui/Popover/Popover"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "../ui/Command/Command"
+import { cn } from "~/src/lib/utils"
 
+// TBD: doc: below code examples show:
+// - how to integrate Combobox with Form
+// - the flexibility to create very similar UI for Phone number input provided by Stripe
 export default () => {
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const [formState, setFormState] = useState<FormState<z.infer<typeof schema>> | undefined>(undefined);
   const shouldStopToNextStep = (formState?.isValidating || formState?.isSubmitting) || !formState?.isValid;
   const schema = z.object({
-    name: z.string().nonempty('The name field cannot be empty.'),
+    name: z.string().nonempty('Required'),
+    email: z.string().nonempty('Required'),
     country: z.string(),
-    adr1: z.string(),
+    adr1: z.string().nonempty('Required'),
     adr2: z.string(),
-    zip: z.string().regex(/^\d+$/, 'Invalid postal code'),
-    city: z.string(),
-    phone: z.string().regex(/^\d+$/),
+    zip: z.string().nonempty('Required'),
+    city: z.string().nonempty('Required'),
+    phone: z.string().nonempty('Required'),
   })
   const defaultValues = {
-    country: '',
+    country: 'us',
   }
   return (
     <div className='tw:flex tw:gap-16 tw:border'>
@@ -60,6 +68,7 @@ export default () => {
             defaultValues={defaultValues}
             setFormState={setFormState}
             // mode="onSubmit"
+
             mode="onChange"
             disabled={isDisabled}
             onSubmit={() => { }}
@@ -69,20 +78,15 @@ export default () => {
               <FormControl><Input placeholder="John Doe" /></FormControl>
             </FormItem>
             <br />
+            <FormItem name='email' className='tw:flex tw:flex-col tw:gap-0.5'>
+              <FormLabel className='tw:pl-1'> Email</FormLabel>
+              <FormControl><Input placeholder="john.doe@example.com" /></FormControl>
+            </FormItem>
+
+            <br />
             <FormItem name='country' className='tw:flex tw:flex-col tw:gap-0.5'>
               <FormLabel className='tw:pl-1'>Address</FormLabel>
-              <FormControl>
-                <Select>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a country" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="us">United States</SelectItem>
-                    <SelectItem value="ca">Canada</SelectItem>
-                    <SelectItem value="mx">Mexico</SelectItem>
-                  </SelectContent>
-                </Select>
-              </FormControl>
+              <FormControl><CountryCombobox /></FormControl>
             </FormItem>
 
             <div className="tw:flex tw:flex-col tw:gap-2 tw:my-2">
@@ -111,9 +115,9 @@ export default () => {
             <br />
             <FormItem name='phone' className='tw:flex tw:flex-col tw:gap-0.5'>
               <FormLabel className='tw:pl-1'>Phone number</FormLabel>
-              <div className='tw:flex tw:items-center tw:gap-3'>
-                <Select>
-                  <SelectTrigger>
+              <div className='tw:flex tw:items-center tw:gap-3 tw:border tw:rounded-md tw:shadow-xs'>
+                <Select disabled={isDisabled}>
+                  <SelectTrigger className='tw:border-none tw:shadow-none tw:w-20'>
                     <SelectValue placeholder="DZ" />
                   </SelectTrigger>
                   <SelectContent className='tw:w-10 tw:flex-0'>
@@ -123,7 +127,7 @@ export default () => {
                 </Select>
                 <div>+253</div>
                 <FormControl>
-                  <Input placeholder="Address line 1" />
+                  <Input placeholder="2 2323 4567" className='tw:border-none tw:shadow-none' />
                 </FormControl>
               </div>
             </FormItem>
@@ -134,5 +138,84 @@ export default () => {
         </section>
       </div>
     </div>
+  )
+}
+
+const countries = [
+  {
+    value: "us",
+    label: "United States",
+  },
+  {
+    value: "ca",
+    label: "Canada",
+  },
+  {
+    value: "mx",
+    label: "Mexico",
+  },
+  {
+    value: "uk",
+    label: "United Kingdom",
+  },
+  {
+    value: "de",
+    label: "Germany",
+  },
+]
+
+const CountryCombobox = ({ value = '', onChange, onBlur, disabled = false }: any) => {
+  const [open, setOpen] = useState(false)
+  const [selected, setSelected] = useState(value)
+  const select = (v: string) => {
+    setSelected(v === selected ? "" : v)
+    setOpen(false)
+    onChange?.(v)
+  }
+  const toggle = (e: boolean) => {
+    setOpen(e)
+    if (!e) onBlur?.()
+  }
+  return (
+    <Popover open={open} onOpenChange={toggle}>
+      <PopoverTrigger asChild>
+        <Cta
+          variant="outline"
+          disabled={disabled}
+          role="combobox"
+          aria-expanded={open}
+          className="tw:w-[300px] tw:justify-between"
+        >
+          {selected
+            ? countries.find((country) => country.value === selected)?.label
+            : "Select country..."}
+          <Icon icon="lucide:chevron-down" className="tw:opacity-50" />
+        </Cta>
+      </PopoverTrigger>
+      <PopoverContent className="tw:w-[300px] tw:p-0">
+        <Command >
+          <CommandInput placeholder="Search country..." className="tw:h-9" />
+          <CommandList>
+            <CommandEmpty>No country found.</CommandEmpty>
+            <CommandGroup>
+              {countries.map((country) => (
+                <CommandItem
+                  key={country.value}
+                  value={country.value}
+                  onSelect={select}
+                >
+                  {country.label}
+                  <Icon icon="lucide:check" className={cn(
+                    "tw:ml-auto",
+                    selected === country.value ? "tw:opacity-100" : "tw:opacity-0"
+                  )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   )
 }
