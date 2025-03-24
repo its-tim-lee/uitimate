c03962d012c3b443d91c366ba9cd2d89800f0995
timlee <hungtington.dev@gmail.com>
2025-03-24 15:12:54 -0400

    feat: add Chart

---
+++ src/components/demo/chart-demo.tsx
@@ -0,0 +1,48 @@
+
+
+
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+
+import { ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent, type ChartConfig, ChartContainer } from "@/components/ui/Chart/Chart"
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)", // this will define a CSS var called `--color-desktop`
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig
+
+
+export default () => {
+  return <ChartContainer config={chartConfig} className="tw-min-h-[200px] tw:w-[50vw]">
+    <BarChart accessibilityLayer data={chartData}>
+      <CartesianGrid vertical={false} />
+      <XAxis
+        dataKey="month"
+        tickLine={false}
+        tickMargin={10}
+        axisLine={false}
+        tickFormatter={(value) => value.slice(0, 3)}
+      />
+      <ChartTooltip content={<ChartTooltipContent />} />
+      <ChartLegend content={<ChartLegendContent verticalAlign='top' />} />
+      <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+      <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+    </BarChart>
+  </ChartContainer>
+};
