4c405f5ac2152fed3514e2f1c1f507f627753f33
timlee <hungtington.dev@gmail.com>
2025-03-30 16:45:28 -0400

    🧹 chore: modify the docs/ from the paste result

--- packages/docs/app/routes.ts
+++ packages/docs/app/routes.ts
@@ -1,31 +1,15 @@
 import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
-
-// export default [
-//   index("routes/home.tsx"),
-//   route("about/", "./routes/about.tsx"),
-//   ...prefix("doc/", [ // path prefix
-//     index("./routes/picking-a-mode.tsx"),
-//     route("routing/", "./routes/routing.tsx"),
-//   ]),
-// ] satisfies RouteConfig;
-
-
 /**
  * - This defines route modules
  * - If only `flatRoutes` is used, simply `export default` that
  * - Every route in routes.ts is nested inside the special app/root.tsx module (Root Route).
  */
 export default [
-  // route('prerender', './routes/prerender.tsx'),
-  route('dashboard', './routes/dashboard.tsx', [
-    index('./routes/dashboard-home.tsx'), // this will be a default route under the parent route, so can be accessed via `dashboard/`
-    // route('settings', './routes/settings.tsx'), // can only be seen under `dashboard/settings`
-  ]),
   ...(await flatRoutes({
-    rootDirectory: "pages",
-    ignoredRouteFiles: ["pages/not-a-route.tsx", "pages/**/*.test.{ts,tsx}"],
+    rootDirectory: "routes",
+    ignoredRouteFiles: ["routes/not-a-route.tsx", "routes/**/*.test.{ts,tsx}"],
   }))
 ] satisfies RouteConfig;
 
