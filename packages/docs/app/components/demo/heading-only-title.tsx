61f856db89d91ecf6d4af294cbb0d4fb4338f889
timlee <hungtington.dev@gmail.com>
2025-04-09 13:38:55 -0400

    ✨ feat: update heading-only-title

--- packages/docs/app/components/demo/heading-only-title.tsx
+++ packages/docs/app/components/demo/heading-only-title.tsx
@@ -17,5 +17,11 @@ export default () => (
     <br />
     <Heading size="h4">H4 Heading Title</Heading>
     <p>Some paragraph, but not subtitle</p>
+    <br />
+    <Heading size="h5">H5 Heading Title</Heading>
+    <p>Some paragraph, but not subtitle</p>
+    <br />
+    <Heading size="h6">H6 Heading Title</Heading>
+    <p>Some paragraph, but not subtitle</p>
   </div>
 )
