f2ff0d686b995227b584bf5702253c96a5aaf13f
timlee <hungtington.dev@gmail.com>
2025-04-10 10:50:56 -0400

    ✨ feat: better Icon doc

---
+++ packages/docs/app/components/ui/Icon/meta.tsx
@@ -0,0 +1,4 @@
+import { Link } from "react-router";
+export default {
+  description: <span>An wrapper of <Link className="tw:link" to="https://iconify.design/docs/icon-components/react/" target="_blank" rel="noopener noreferrer">@iconify/react</Link> to provide an accessible SVG icon.</span>,
+}
