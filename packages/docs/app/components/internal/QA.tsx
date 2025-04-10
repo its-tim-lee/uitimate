d7129408b5acf4f0661a5f12a323c16a92d17349
timlee <hungtington.dev@gmail.com>
2025-04-10 09:04:31 -0400

    ✨ feat: a better QA component

--- packages/docs/app/components/internal/QA.tsx
+++ packages/docs/app/components/internal/QA.tsx
@@ -1,11 +1,11 @@
 import type { ReactNode } from "react";
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "#/components/ui/Accordion/Accordion";
 type QAProps = {
-  items: { value: string, trigger: string, content: ReactNode }[]
+  items: { value: string, trigger: ReactNode, content: ReactNode }[]
 }
 export default ({ items }: QAProps) => {
   return (
-    <Accordion type="single" collapsible className="tw:w-[80%] tw:mx-auto">
+    <Accordion type="single" collapsible className="tw:w-[80%] tw:mx-auto ">
       {items.map($i => (
         <AccordionItem key={$i.value} value={$i.value}>
           <AccordionTrigger>{$i.trigger}</AccordionTrigger>
