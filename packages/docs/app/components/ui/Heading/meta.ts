882809202db587db7023d05a2e82ccf8dd87b017
timlee <hungtington.dev@gmail.com>
2025-04-10 10:47:53 -0400

    ✨ feat: a better Heading doc

--- packages/docs/app/components/ui/Heading/meta.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  description: 'Heading is a title and an optional subtitle blend together to form the concept of "heads up" for readers that getting more attention than the normal text.',
-}
---
+++ packages/docs/app/components/ui/Heading/meta.tsx
@@ -0,0 +1,3 @@
+export default {
+  description: 'A title and an optional subtitle blend together to form the concept of "heads up" for readers that getting more attention than the normal text.',
+}
