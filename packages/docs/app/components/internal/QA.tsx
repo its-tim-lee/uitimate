35743b78c2fb82fc0f38a0d0c65c3c931a0884c1
timlee <hungtington.dev@gmail.com>
2025-04-10 06:33:24 -0400

    ✨ feat: better Heading

---
+++ packages/docs/app/components/internal/QA.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from "react";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "#/components/ui/Accordion/Accordion";
+type QAProps = {
+  items: { value: string, trigger: string, content: ReactNode }[]
+}
+export default ({ items }: QAProps) => {
+  return (
+    <Accordion type="single" collapsible className="tw:w-[80%] tw:mx-auto">
+      {items.map($i => (
+        <AccordionItem key={$i.value} value={$i.value}>
+          <AccordionTrigger>{$i.trigger}</AccordionTrigger>
+          <AccordionContent>{$i.content}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
