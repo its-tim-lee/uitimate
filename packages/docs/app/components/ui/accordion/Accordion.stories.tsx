c9e72f1d888dacee07b4897f5ceda67bc4ef4b6a
timlee <hungtington.dev@gmail.com>
2025-04-11 11:36:02 -0400

    ✨ feat: accordion should be ready

--- packages/docs/app/components/ui/accordion/Accordion.stories.tsx
+++ packages/docs/app/components/ui/accordion/Accordion.stories.tsx
@@ -4,6 +4,14 @@ import AccordionOverview from '#/components/demo/accordion-overview.tsx';
 
 export default {
   title: 'Example/Accordion',
+  /**
+   * TODO: the complete testing suites.
+   * That's hard by hand, but totally doable by AI.
+   * For this dynamic component, visual testing + behavior testing are needed.
+   * Now we only test the visual one, and choosing "Overview" as the major test is becausse
+   * it involves many common scenarios.
+   */
+  includeStories: ['Overview'],
   parameters: {
     layout: 'centered',
   },
@@ -18,7 +26,7 @@ export const Demo = {
   render: () => <AccordionDemo />
 };
 
-export const Multiple = {
+export const Overview = {
   name: 'API / Overview',
   render: () => <AccordionOverview />
 };
