3c8dfb23112cef1b952e4c9e668b07ba1ef571d6
timlee <hungtington.dev@gmail.com>
2025-04-11 10:11:18 -0400

    ✨ feat: better api doc management

---
+++ packages/docs/app/components/internal/ApiDoc.tsx
@@ -0,0 +1,54 @@
+import {
+  Heading,
+  HeadingSubtitle,
+  HeadingTitle,
+} from "#/components/ui/Heading/Heading.tsx";
+import { Link as ScrollLink } from "react-scroll";
+
+const UsageSection = () => {
+  return (
+    <Heading size="h2" id="usage">
+      <HeadingTitle>Usage</HeadingTitle>
+      <HeadingSubtitle>How you can play this component.</HeadingSubtitle>
+    </Heading>
+  )
+}
+
+const DemoApiSection = () => {
+  return (
+    <Heading size="h2" id='demo-api'>
+      <HeadingTitle>DEMO / API</HeadingTitle>
+      <HeadingSubtitle>Trying to show all the common API/Component usages below, but this can be insufficient/impossible in some cases, and we'd then use
+        {' '}
+        <ScrollLink
+          to="demo-scenarios"
+          spy={true}
+          smooth={true}
+          offset={-20}
+          duration={0}
+          className="tw:link tw:cursor-pointer"
+        >DEMO / Scenario</ScrollLink> to express more.</HeadingSubtitle>
+    </Heading>
+  )
+}
+
+const DemoScenariosSection = () => {
+  return (
+    <Heading size="h2" id="demo-scenarios">
+      <HeadingTitle>DEMO / Scenario</HeadingTitle>
+      <HeadingSubtitle>This lists common real-world use cases.</HeadingSubtitle>
+    </Heading>
+  )
+}
+
+const DependenciesSection = () => {
+  return (
+    <Heading size="h2" id="dependencies">
+      <HeadingTitle>Dependencies</HeadingTitle>
+      <HeadingSubtitle>Below lists out the 3rd-party libaries that this component depends on.</HeadingSubtitle>
+    </Heading>
+  )
+}
+export {
+  UsageSection, DemoApiSection, DemoScenariosSection, DependenciesSection
+}
