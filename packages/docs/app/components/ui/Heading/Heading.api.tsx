4bbcf32166215bde58e4e0ff6328b91a373db42e
timlee <hungtington.dev@gmail.com>
2025-04-11 10:11:18 -0400

    ✨ feat: better api doc management

--- packages/docs/app/components/ui/Heading/Heading.api.tsx
+++ packages/docs/app/components/ui/Heading/Heading.api.tsx
@@ -3,23 +3,24 @@ import {
   HeadingSubtitle,
   HeadingTitle,
 } from "./Heading.tsx";
+import {
+  UsageSection,
+  DemoApiSection,
+  DemoScenariosSection,
+} from "#/components/internal/ApiDoc.tsx";
 import VersatileTabs from "#/components/internal/VersatileTabs.tsx";
 import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
 import meta from "./Heading.meta.tsx";
 import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
-import { Link as ScrollLink } from "react-scroll";
+
 export default () => {
   return (
     <>
       <ComponentPageHero title='API' subtitle={meta.description} />
 
-
       <br />
 
-      <Heading size="h2" id="usage">
-        <HeadingTitle>Usage</HeadingTitle>
-        <HeadingSubtitle>How you can play this component.</HeadingSubtitle>
-      </Heading>
+      <UsageSection />
       <ComponentPageUsage
         demoId="heading-demo"
         anatomy={meta.anatomy}
@@ -27,19 +28,8 @@ export default () => {
 
       <br />
       <br />
-      <Heading size="h2" id='demo-api'>
-        <HeadingTitle>DEMO / API</HeadingTitle>
-      <HeadingSubtitle>Trying to show all the common API/Component usages below, but this can be insufficient/impossible in some cases, and we'd then use
-        {' '}
-        <ScrollLink
-          to="demo-scenarios"
-          spy={true}
-          smooth={true}
-          offset={-20}
-          duration={0}
-          className="tw:link tw:cursor-pointer"
-        >DEMO / Scenario</ScrollLink> to express more.</HeadingSubtitle>
-    </Heading >
+
+      <DemoApiSection />
       <VersatileTabs
         settings={[
           {
@@ -58,10 +48,7 @@ export default () => {
       <br />
       <br />
 
-      <Heading size="h2" id="demo-scenarios">
-          <HeadingTitle>DEMO / Scenario</HeadingTitle>
-          <HeadingSubtitle>This lists common real-world use cases.</HeadingSubtitle>
-        </Heading>
+      <DemoScenariosSection />
       <VersatileTabs
         settings={[
           {
--- packages/docs/app/components/ui/Icon/Icon.api.tsx
+++ packages/docs/app/components/ui/Icon/Icon.api.tsx
@@ -1,25 +1,23 @@
 import {
-  Heading,
-  HeadingSubtitle,
-  HeadingTitle,
-} from "#/components/ui/Heading/Heading.tsx";
+  UsageSection,
+  DemoApiSection,
+  DemoScenariosSection,
+  DependenciesSection,
+} from "#/components/internal/ApiDoc.tsx";
 import VersatileTabs from "#/components/internal/VersatileTabs.tsx";
 import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
 import meta from "./Icon.meta.tsx";
 import { Link } from "react-router";
 import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
+
 export default () => {
   return (
     <>
-
       <ComponentPageHero title='API' subtitle={meta.description} />
 
       <br />
 
-      <Heading size="h2" id="usage">
-        <HeadingTitle>Usage</HeadingTitle>
-        <HeadingSubtitle>How you can play this component.</HeadingSubtitle>
-      </Heading>
+      <UsageSection />
       <ComponentPageUsage
         demoId="icon-size"
       />
@@ -27,10 +25,7 @@ export default () => {
       <br />
       <br />
 
-      <Heading size="h2" id="demo-scenarios">
-        <HeadingTitle>DEMO / Scenario</HeadingTitle>
-        <HeadingSubtitle>This lists common real-world use cases.</HeadingSubtitle>
-      </Heading>
+      <DemoScenariosSection />
       <VersatileTabs
         settings={[
           {
@@ -44,12 +39,7 @@ export default () => {
       <br />
       <br />
 
-      <Heading size="h2" id="dependencies">
-        <HeadingTitle>Dependencies</HeadingTitle>
-        <HeadingSubtitle>Below lists out the 3rd-party libaries that this component depends on.</HeadingSubtitle>
-      </Heading>
-
-
+      <DependenciesSection />
       <Link className="tw:link tw:w-fit" to="https://iconify.design/docs/icon-components/react/" target="_blank" rel="noopener noreferrer">@iconify/react</Link>
       <br />
       <br />
