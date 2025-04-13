50beb71b305289dfee625295838b91680cacc669
timlee <hungtington.dev@gmail.com>
2025-04-13 10:15:31 -0400

    ✨ feat: cta rearraged

--- packages/docs/app/components/demo/cta-demo.tsx
+++ packages/docs/app/components/demo/cta-demo.tsx
@@ -5,15 +5,15 @@ export default () => {
   return <div className="tw:flex tw:flex-col tw:gap-4">
     <div className="tw:flex tw:gap-2 tw:items-center">
       It can be a button: {''}
-      <Cta variant="secondary">Download</Cta>
+      <Cta>Download</Cta>
     </div>
     <div className="tw:flex tw:gap-2 tw:items-center">
       or be a badge: {''}
-      <Cta muted variant="outline" shapes={['badge']} className='tw:text-green-500'>Latest</Cta>
+      <Cta muted variant="secondary" shapes={['badge']} className='tw:text-green-500'>Latest</Cta>
     </div>
     <div className="tw:flex tw:gap-2 tw:items-center">
       or a toggle: {''}
-      <Cta shapes={['icon']} size="sm" defaultPressed>
+      <Cta shapes={['icon']} variant="outline" size="sm" defaultPressed>
         <Icon icon="lucide:heart" className='tw:fill-red-500!' />
       </Cta>
     </div>
