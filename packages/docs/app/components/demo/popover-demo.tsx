64234d702073d8ff7a2b1ee8b299358cecc72d9a
timlee <hungtington.dev@gmail.com>
2025-04-13 18:02:54 -0400

    ✨ feat: finish Popover

--- packages/docs/app/components/demo/popover-demo.tsx
+++ packages/docs/app/components/demo/popover-demo.tsx
@@ -1,21 +1,18 @@
-import { Button } from "#/components/ui/Button/Button"
-import { Input } from "#/components/ui/Input/Input"
-import { Label } from "#/components/ui/Label/Label"
+import { Cta } from "#/components/ui/Cta/Cta"
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "#/components/ui/Popover/Popover"
-import { Flat } from "../preset/flat"
-import { Icon } from "../ui/Icon/Icon"
-import { Separator } from "../ui/Separator/Separator"
-import { Checkbox } from "../ui/Checkbox/Checkbox"
+import { Icon } from "#/components/ui/Icon/Icon"
+import { Separator } from "#/components/ui/Separator/Separator"
+import { Checkbox } from "#/components/ui/Checkbox/Checkbox"
 
 export default () => {
   return (
     <Popover>
       <PopoverTrigger asChild>
-        <Button className='tw:shadow-none' variant="outline" size='sm'>Notify me: <b>on Github, Email</b></Button>
+        <Cta className='tw:shadow-none' variant="outline" size='sm'>Notify me: <b>on Github, Email</b></Cta>
       </PopoverTrigger>
       <PopoverContent className='tw:w-85'>
         <header className="tw:flex tw:items-center tw:justify-between">
@@ -33,8 +30,8 @@ export default () => {
         </div>
         <Separator className="tw:my-4" />
         <div className="tw:flex tw:justify-end tw:gap-2">
-          <Button variant="outline" size='sm' className='tw:shadow-none'>Cancel</Button>
-          <Button variant="primary" size='sm' className='tw:shadow-none'>Save</Button>
+          <Cta variant="outline" size='sm' className='tw:shadow-none'>Cancel</Cta>
+          <Cta variant="primary" size='sm' className='tw:shadow-none'>Save</Cta>
         </div>
       </PopoverContent>
     </Popover>
