24359790fb349193ce8f8ecdabb1e216bf1ed3d1
timlee <hungtington.dev@gmail.com>
2025-04-13 15:00:58 -0400

    ✨ feat: better api doc of Avatar

--- packages/docs/app/components/ui/Avatar/Avatar.meta.tsx
+++ packages/docs/app/components/ui/Avatar/Avatar.meta.tsx
@@ -1,5 +1,11 @@
 const meta = {
   description: "An image element with a fallback for representing the user.",
+  anatomy: `
+<Avatar>
+  <AvatarImage />
+  <AvatarFallback />
+</Avatar>
+  `
 }
 
 export default meta
