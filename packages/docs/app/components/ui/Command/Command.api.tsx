2bab98619ac6f6ebe11d97f1544266a843d9d2dc
timlee <hungtington.dev@gmail.com>
2025-04-13 22:52:19 -0400

    ✨ feat: finish Command

---
+++ packages/docs/app/components/ui/Command/Command.api.tsx
@@ -0,0 +1,45 @@
+import {
+  UsageSection,
+  DemoScenariosSection,
+  DependenciesSection,
+} from "#/components/internal/ApiDoc.tsx";
+import VersatileTabs from "#/components/internal/VersatileTabs.tsx";
+import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
+import meta from "./Command.meta.tsx";
+import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
+import { Link } from "react-router";
+
+export default () => {
+  return (
+    <>
+      <ComponentPageHero title='API' subtitle={meta.description} />
+
+      <br />
+
+      <UsageSection />
+      <ComponentPageUsage
+        demoId="command-demo"
+        anatomy={meta.anatomy}
+      />
+
+      <br />
+      <br />
+
+      <DemoScenariosSection />
+      <VersatileTabs
+        settings={[
+          {
+            title: "Command Dialog",
+            type: "preview",
+            demoId: "command-dialog",
+          }
+        ]}
+      />
+      <br />
+      <br />
+
+      <DependenciesSection />
+      <Link className="tw:link tw:w-fit" to="https://github.com/pacocoursey/cmdk" target="_blank" rel="noopener noreferrer">cmdk</Link>
+    </>
+  )
+}
