4498b549b6dfbfd8a215bd7a63f62ab1722b5ab7
timlee <hungtington.dev@gmail.com>
2025-04-14 06:39:51 -0400

    ✨ feat: finish contextmenu

---
+++ packages/docs/app/components/ui/ContextMenu/ContextMenu.meta.tsx
@@ -0,0 +1,14 @@
+export default {
+  description: 'Displays a vertical menu when right-clicking for managing a consistent set of commands via a hierarchical structure',
+  anatomy: `
+{/*
+  The basic structure is like below;
+  things can be complex inside the <ContextMenuContent>,
+  so refer to the relevant demos to see the usages.
+*/}
+<ContextMenu>
+  <ContextMenuTrigger/>
+  <ContextMenuContent/>
+</ContextMenu>
+  `,
+}
