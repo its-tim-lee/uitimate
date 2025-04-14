3ee11f82a1a0f12384cf4993e7e3eb632a585297
timlee <hungtington.dev@gmail.com>
2025-04-14 12:15:30 -0400

    ✨ feat: finish breadcrumb

--- packages/docs/app/components/demo/breadcrumb-expansible-collapse.tsx
+++ packages/docs/app/components/demo/breadcrumb-expansible-collapse.tsx
@@ -1,12 +1,82 @@
-import { Breadcrumb, BreadcrumbEllipsis, BreadcrumbFinal, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from "#/components/ui/Breadcrumb/Breadcrumb.tsx";
-import { DrawerContent } from "#/components/ui/Drawer/Drawer.tsx";
-import { DrawerTrigger } from "#/components/ui/Drawer/Drawer.tsx";
-import { Drawer } from "#/components/ui/Drawer/Drawer.tsx";
-import { DropdownMenuItem } from "#/components/ui/DropdownMenu/DropdownMenu.tsx";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "#/components/ui/DropdownMenu/DropdownMenu.tsx";
-import { Icon } from "#/components/ui/Icon/Icon.tsx";
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbFinal,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator
+} from "#/components/ui/Breadcrumb/Breadcrumb";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeading,
+  DrawerTitle,
+  DrawerTrigger
+} from "#/components/ui/Drawer/Drawer";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "#/components/ui/DropdownMenu/DropdownMenu";
+import { Icon } from "#/components/ui/Icon/Icon";
+import { useState } from "react";
+import { useIsMobile } from "~/app/helpers/hooks/use-mobile";
+
+const BREADCRUMB_ITEMS = [
+  { href: "/docs", text: "Documentation" },
+  { href: "/docs/performance-monitoring", text: "Performance Monitoring" },
+  { href: "/docs/get-started", text: "Get started" }
+];
+
+const EnpanededBreadcrumbEllipsis = ({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: (isOpen: boolean) => void }) => {
+  const isMobile = useIsMobile();
+  if (isMobile) {
+    return (
+      <Drawer open={isOpen} onOpenChange={setIsOpen}>
+        <DrawerTrigger asChild><span /></DrawerTrigger>
+        <DrawerContent className="tw:p-3">
+          <DrawerHeading size="h5">
+            Navigate to:
+          </DrawerHeading>
+          <div className="tw:grid tw:gap-3">
+            {BREADCRUMB_ITEMS.map((item) => (
+              <a
+                href={item.href}
+                className="tw:py-1 tw:text-sm"
+                key={item.href}
+                onClick={() => setIsOpen(false)}
+              >
+                {item.text}
+              </a>
+            ))}
+          </div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenuTrigger asChild>
+        <span />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {BREADCRUMB_ITEMS.map((item) => (
+          <DropdownMenuItem
+            key={item.href}
+            onClick={() => setIsOpen(false)}
+          >
+            {item.text}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+};
 
 export default () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Breadcrumb>
 
@@ -14,56 +84,28 @@ export default () => {
         <BreadcrumbLink href="/">Firebase</BreadcrumbLink>
       </BreadcrumbItem>
 
-      <BreadcrumbSeparator><Icon icon="lucide:slash" /></BreadcrumbSeparator>
+      <BreadcrumbSeparator>
+        <Icon icon="lucide:slash" />
+      </BreadcrumbSeparator>
 
-      {/* Desktop Style */}
-      <BreadcrumbItem className='tw:hidden tw:sm:block'>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <BreadcrumbEllipsis />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>
-              Documentation
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Performance Monitoring
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Get started
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+      <BreadcrumbItem>
+        <BreadcrumbLink asChild><a href='/docs'>Documentation</a></BreadcrumbLink>
       </BreadcrumbItem>
 
-      {/* Mobile Style */}
-      <BreadcrumbItem className='tw:block tw:sm:hidden'>
-        <Drawer>
-          <DrawerTrigger asChild><BreadcrumbEllipsis /></DrawerTrigger>
-          <DrawerContent>
-            Navigate to:
+      <BreadcrumbSeparator><Icon icon="lucide:slash" /></BreadcrumbSeparator>
 
-            <div className="tw:grid tw:gap-3 tw:px-4">
-              {[
-                { href: "/docs", text: "Documentation" },
-                { href: "/docs/performance-monitoring", text: "Performance Monitoring" },
-                { href: "/docs/get-started", text: "Get started" }
-              ].map((item) => (
-                <a href={item.href} className="tw:py-1 tw:text-sm" key={item.href}>
-                  {item.text}
-                </a>
-              ))}
-            </div>
-          </DrawerContent>
-        </Drawer>
+      {/* Too more items, show ellipsis to express "collapse" */}
+      <BreadcrumbItem>
+        <BreadcrumbEllipsis onClick={() => setIsOpen(!isOpen)} className="tw:cursor-pointer" />
+        <EnpanededBreadcrumbEllipsis isOpen={isOpen} setIsOpen={setIsOpen} />
       </BreadcrumbItem>
 
       <BreadcrumbSeparator><Icon icon="lucide:slash" /></BreadcrumbSeparator>
 
       <BreadcrumbItem>
-        <BreadcrumbFinal>Web</BreadcrumbFinal>
+        <BreadcrumbFinal>Crashlytics</BreadcrumbFinal>
       </BreadcrumbItem>
 
     </Breadcrumb>
-  )
+  );
 }
---
+++ packages/docs/app/components/ui/Breadcrumb/Breadcrumb.meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  description: 'A navigation aid to indicate the current location within a website\'s hierarchy and navigate between levels.',
+  anatomy: `
+<Breadcrumb>
+  <BreadcrumbItem/>
+  <BreadcrumbSeparator/>
+  {/* more <BreadcrumbItem> and <BreadcrumbSeparator> */}
+</Breadcrumb>
+  `,
+}
