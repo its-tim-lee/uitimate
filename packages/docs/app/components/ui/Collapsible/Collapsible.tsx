c2cdb667c2220533e83b866294bbc4500bc103e0
timlee <hungtington.dev@gmail.com>
2025-04-14 17:23:37 -0400

    ✨ feat: finish Collapsible

--- packages/docs/app/components/ui/Collapsible/Collapsible.tsx
+++ packages/docs/app/components/ui/Collapsible/Collapsible.tsx
@@ -1,9 +1,39 @@
-import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+import type { CollapsibleProps, CollapsibleTriggerProps, CollapsibleContentProps } from "@radix-ui/react-collapsible"
+import * as Primitive from "@radix-ui/react-collapsible"
+import { kebabCase } from "lodash-es";
 
-const Collapsible = CollapsiblePrimitive.Root
+const Collapsible =
+  ({ ...props }: CollapsibleProps) => (
+    <Primitive.Root
+      data-tag={kebabCase(Collapsible.displayName)}
+      {...props}
+    />
+  );
 
-const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
+const CollapsibleTrigger =
+  ({ ...props }: CollapsibleTriggerProps) => (
+    <Primitive.CollapsibleTrigger
+      data-tag={kebabCase(CollapsibleTrigger.displayName)}
+      {...props}
+    />
+  );
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent =
+  ({ ...props }: CollapsibleContentProps) => (
+    <Primitive.CollapsibleContent
+      data-tag={kebabCase(CollapsibleContent.displayName)}
+      {...props}
+    />
+  );
 
-export { Collapsible, CollapsibleTrigger, CollapsibleContent }
+Collapsible.displayName = 'Collapsible'
+CollapsibleTrigger.displayName = 'CollapsibleTrigger'
+CollapsibleContent.displayName = 'CollapsibleContent'
+
+namespace Type {
+  export type Collapsible = CollapsibleProps
+  export type CollapsibleTrigger = CollapsibleTriggerProps
+  export type CollapsibleContent = CollapsibleContentProps
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent, type Type }
