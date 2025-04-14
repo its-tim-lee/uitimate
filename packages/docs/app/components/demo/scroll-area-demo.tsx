4453357bbf9208b513f64a0a73dffcb3ef43bd0b
timlee <hungtington.dev@gmail.com>
2025-04-14 12:15:42 -0400

    ✨ feat: update some demos

--- packages/docs/app/components/demo/scroll-area-demo.tsx
+++ packages/docs/app/components/demo/scroll-area-demo.tsx
@@ -1,4 +1,4 @@
-import { ScrollArea } from "../ui/ScollArea/ScollArea";
+import { ScrollArea } from "#/components/ui/ScollArea/ScollArea";
 
 const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`)
 
