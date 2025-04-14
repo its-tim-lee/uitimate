45460a4539f942ee5de97417c57bd15dbf75141b
timlee <hungtington.dev@gmail.com>
2025-04-14 17:09:10 -0400

    ✨ feat: create an information banner component For internal use

---
+++ packages/docs/app/components/internal/InfoBanner.tsx
@@ -0,0 +1,12 @@
+import { type ComponentProps } from "react"
+import { Flat } from "#/components/preset/flat"
+import { Icon } from "#/components/ui/Icon/Icon"
+
+export default ({ children }: ComponentProps<'p'>) => {
+  return (
+    <Flat size="sm" className="tw:flex tw:items-center tw:gap-2 tw:border-primary">
+      <Icon icon='lucide:info' className='tw:mt-1 tw:size-5' />
+      <p>{children}</p>
+    </Flat >
+  )
+}
