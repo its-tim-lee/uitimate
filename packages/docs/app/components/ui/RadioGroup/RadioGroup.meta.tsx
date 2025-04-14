09cfcd052524216c3fe9a333667cb391934923f7
timlee <hungtington.dev@gmail.com>
2025-04-14 07:54:59 -0400

    ✨ feat: finish RadioGroup

---
+++ packages/docs/app/components/ui/RadioGroup/RadioGroup.meta.tsx
@@ -0,0 +1,11 @@
+const meta = {
+  description: "A set of checkable buttons—known as radio buttons—where no more than one of the buttons can be checked at a time.",
+  anatomy: `
+    <RadioGroup>
+      <RadioGroupItem/>
+      {/* More <RadioGroupItem>... */}
+    </RadioGroup>
+  `
+}
+
+export default meta
