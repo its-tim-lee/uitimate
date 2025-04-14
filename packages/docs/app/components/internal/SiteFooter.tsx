a2f8f875756cdafbad69a51ed550438de0528384
timlee <hungtington.dev@gmail.com>
2025-04-14 15:42:33 -0400

    ✨ feat: fix site footer

--- packages/docs/app/components/internal/SiteFooter.tsx
+++ packages/docs/app/components/internal/SiteFooter.tsx
@@ -2,10 +2,10 @@ import siteData from "#/data/site";
 
 const SiteFooter = () => {
   return (
-    <footer className="border-grid tw:border-t tw:py-6 tw:md:py-0">
+    <footer className="border-grid tw:border-t tw:py-6 tw:md:py-0 tw:relative tw:bottom-0 tw:w-full tw:z-50 tw:bg-background">
       <div className="container-wrapper">
-        <div className="container tw:py-4">
-          <div className="tw:text-balance tw:text-center tw:text-sm tw:leading-loose tw:text-muted-foreground tw:md:text-left">
+        <div className="container tw:py-4 ">
+          <div className="tw:text-balance tw:text-center tw:text-sm tw:leading-loose tw:text-muted-foreground">
             Made with ❤️ by{" "}
             <a
               href={siteData.twitter}
