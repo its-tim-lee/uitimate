ef92cb173a4de6e0c594ed7bf2f130d06879cd71
timlee <hungtington.dev@gmail.com>
2025-04-14 07:44:39 -0400

    ✨ feat: finish dropdownmenu

---
+++ packages/docs/app/components/ui/DropdownMenu/DropdownMenu.api.tsx
@@ -0,0 +1,51 @@
+import {
+  UsageSection,
+  DemoScenariosSection,
+  DependenciesSection,
+  DemoRecipeSection,
+} from "#/components/internal/ApiDoc.tsx";
+import VersatileTabs from "#/components/internal/VersatileTabs.tsx";
+import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
+import meta from "./DropdownMenu.meta.tsx";
+import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
+import { Link } from "react-router";
+
+export default () => {
+  return (
+    <>
+      <ComponentPageHero title='API' subtitle={meta.description} />
+
+      <br />
+
+      <UsageSection />
+      <ComponentPageUsage
+        demoId="dropdownmenu-demo"
+        anatomy={meta.anatomy}
+      />
+
+      <br />
+      <br />
+
+      <DemoScenariosSection />
+      <VersatileTabs
+        settings={[
+          {
+            title: "Common",
+            type: "preview",
+            demoId: "dropdownmenu-mix1",
+          },
+          {
+            title: "Checkbox & Radio",
+            type: "preview",
+            demoId: "dropdownmenu-mix2",
+          },
+        ]}
+      />
+      <br />
+      <br />
+
+      <DependenciesSection />
+      <Link className="tw:link tw:w-fit" to="https://www.radix-ui.com/primitives/docs/components/dropdown-menu" target="_blank" rel="noopener noreferrer">@radix-ui/react-dropdown-menu</Link>
+    </>
+  )
+}
