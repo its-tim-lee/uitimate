5dbca3ff44e46b91a88cd5c55afb66d6e3ff6199
timlee <hungtington.dev@gmail.com>
2025-04-15 16:13:57 -0400

    ✨ feat: make use-mobile SSR-able

--- packages/docs/app/helpers/hooks/use-mobile.tsx
+++ packages/docs/app/helpers/hooks/use-mobile.tsx
@@ -3,9 +3,10 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(window.innerWidth < MOBILE_BREAKPOINT)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
@@ -14,5 +15,5 @@ export function useIsMobile() {
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile ?? false // Default to false during SSR
 }
