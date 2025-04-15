0b42b91d48b4f163352999e31d53b051a7c3384e
timlee <hungtington.dev@gmail.com>
2025-04-15 16:20:03 -0400

    ✨ feat: finish Drawer

---
+++ packages/docs/app/components/ui/Drawer/Drawer.meta.tsx
@@ -0,0 +1,12 @@
+export default {
+  description: 'A panel that slides out from the edge of the screen, mostly used as a dialog replacement on mobile devices.',
+  anatomy: `
+    <Drawer>
+      <DrawerHeading> {/* <-- This is actually an optional */}
+        <DrawerTitle/>
+        <DrawerSubtitle/>
+      </DrawerHeading>
+      {/* Content */}
+    </Drawer>
+  `,
+}
