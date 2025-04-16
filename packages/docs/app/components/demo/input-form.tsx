99bb0b89a3a2298bc955386b84cda48e3faccbed
timlee <hungtington.dev@gmail.com>
2025-04-16 06:04:25 -0400

    ✨ feat: finish Input

--- packages/docs/app/components/demo/input-form.tsx
+++ packages/docs/app/components/demo/input-form.tsx
@@ -1,11 +1,5 @@
-import { Label } from '../ui/Label/Label.tsx';
-import { Input } from '../ui/Input/Input.tsx';
-import { Button } from '../improper/Button/Button.tsx';
+import { Input } from '#/components/ui/Input/Input.tsx';
 
-// TBD: doc: show the intergration with form and leave the msg:
-// since it's just a native input, for the APIs, consult the native input doc for more details
-
-// This demo is still important, cuz it can text many types of Input to see whether our style has any issues
 export default () => {
   return (
     <Input
