5d6013feb135e01052942d6e5be774e1e2c8a858
timlee <hungtington.dev@gmail.com>
2025-04-16 16:18:30 -0400

    ✨ feat: making a copy work in a code block

--- packages/docs/app/components/internal/CodeBlock.tsx
+++ packages/docs/app/components/internal/CodeBlock.tsx
@@ -3,15 +3,32 @@ import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
 import { Cta } from "#/components/ui/Cta/Cta"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { Icon } from "#/components/ui/Icon/Icon"
+import { useState } from "react"
 
 export const CodeBlock = (props: ComponentProps<typeof SyntaxHighlighter>) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopy = async () => {
+    if (typeof props.children === 'string') {
+      await navigator.clipboard.writeText(props.children)
+      setIsCopied(true)
+      setTimeout(() => setIsCopied(false), 2000)
+    }
+  }
+
   return (
     <div className='tw:relative'>
       <SyntaxHighlighter language="tsx" style={vscDarkPlus} {...props}>
         {props.children}
       </SyntaxHighlighter>
-      <Cta size='sm' variant='ghost' shapes={['icon']} className='tw:absolute tw:top-2 tw:right-2 tw:shadow-none tw:bg-transparent tw:text-white tw:dark:text-black'>
-        <Icon icon='lucide:copy' />
+      <Cta
+        size='sm'
+        variant='ghost'
+        shapes={['icon']}
+        className='tw:absolute tw:top-2 tw:right-2 tw:shadow-none tw:bg-transparent tw:text-white tw:dark:text-black'
+        onClick={handleCopy}
+      >
+        <Icon icon={isCopied ? 'lucide:check' : 'lucide:copy'} />
       </Cta>
     </div>
   )
