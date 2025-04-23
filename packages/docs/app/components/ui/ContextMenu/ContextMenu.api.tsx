6b307e6490900e4a2b5a07ba1e69ea33dbe4dbf1
timlee <hungtington.dev@gmail.com>
2025-04-23 12:53:47 -0400

    ✨ feat: make Context Menu Grade

--- packages/docs/app/components/ui/ContextMenu/ContextMenu.api.tsx
+++ packages/docs/app/components/ui/ContextMenu/ContextMenu.api.tsx
@@ -1,29 +1,19 @@
 import {
-  UsageSection,
-  DemoScenariosSection,
   DependenciesSection,
-  DemoRecipeSection,
 } from "#/components/internal/ApiDoc.tsx";
 import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
 import meta from "./ContextMenu.meta.tsx";
-import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
 import { Link } from "react-router";
 
 export default () => {
   return (
     <>
-      <ComponentPageHero title='API' subtitle={meta.description} />
-
-      <br />
-
-      <UsageSection />
       <ComponentPageUsage
         demoId="contextmenu-demo"
+        enableHeading
         anatomy={meta.anatomy}
       />
 
-      <br />
-      <br />
 
       <DependenciesSection />
       <Link className="tw:link tw:w-fit" to="https://www.radix-ui.com/primitives/docs/components/context-menu" target="_blank" rel="noopener noreferrer">@radix-ui/react-context-menu</Link>
