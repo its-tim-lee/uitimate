1821283296de22387678a644348ed7f8a08dc1d0
timlee <hungtington.dev@gmail.com>
2025-04-23 12:21:52 -0400

    ✨ feat: make Avatar great

--- packages/docs/app/components/demo/cta-rounded-badge.tsx
+++ packages/docs/app/components/demo/cta-rounded-badge.tsx
@@ -1,7 +1,7 @@
 import { Avatar, AvatarFallback, AvatarImage } from "#/components/ui/Avatar/Avatar";
 import { Cta } from "../ui/Cta/Cta";
 
-export default () => <div className="tw:flex tw:flex-col tw:items-start">
+export default () => <div className="tw:flex tw:flex-col tw:items-start not-prose">
   <Cta variant="secondary" shapes={['badge']} size='lg' className='tw:rounded-full tw:py-0 tw:pl-0'>
     <Avatar>
       <AvatarImage src='https://bitl.to/44ls' alt="@itistimlee" />
--- packages/docs/app/components/ui/Avatar/Avatar.api.tsx
+++ packages/docs/app/components/ui/Avatar/Avatar.api.tsx
@@ -1,42 +1,27 @@
 import {
-  UsageSection,
   DemoScenariosSection,
   DependenciesSection,
 } from "#/components/internal/ApiDoc.tsx";
-import VersatileTabs from "#/components/internal/VersatileTabs.tsx";
+import { VersatileTabs2, VersatileTabs2Content } from "#/components/internal/VersatileTabs2.tsx";
 import ComponentPageUsage from "#/components/internal/ComponentPageUsage.tsx";
 import meta from "./Avatar.meta.tsx";
-import ComponentPageHero from "#/components/internal/ComponentPageHero.tsx";
 import { Link } from "react-router";
 
 export default () => {
   return (
     <>
-      <ComponentPageHero title='API' subtitle={meta.description} />
-
-      <br />
-
-      <UsageSection />
       <ComponentPageUsage
         demoId="avatar-demo"
+        enableHeading
         anatomy={meta.anatomy}
       />
 
-      <br />
-      <br />
 
       <DemoScenariosSection />
-      <VersatileTabs
-        settings={[
-          {
-            title: "Avatar in Badge",
-            type: "preview",
-            demoId: "cta-rounded-badge",
-          },
-        ]}
-      />
-      <br />
-      <br />
+      <VersatileTabs2 defaultValue="cta-rounded-badge" variant="pill">
+        <VersatileTabs2Content value="cta-rounded-badge" demoId="cta-rounded-badge" />
+      </VersatileTabs2>
+
 
       <DependenciesSection />
       <Link className="tw:link tw:w-fit" to="https://www.radix-ui.com/primitives/docs/components/avatar" target="_blank" rel="noopener noreferrer">@radix-ui/react-avatar</Link>
