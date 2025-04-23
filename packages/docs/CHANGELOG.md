b99927ae9e7bff7baccfaf2b702186e08f3bbe99
timlee <hungtington.dev@gmail.com>
2025-04-23 14:53:39 -0400

    ✨ feat: update changelog and add changelog display component

--- packages/docs/CHANGELOG.md
+++ packages/docs/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+> Note that, any change to the entire site will contribute to this changelog, and it's maintained independently from the component's.
+> (ie., any component source code change will contribute the component's changelog)
+>
+> So, DO NOT CONFUSED!
+
 ## 1.0.0
 First version that serves as the minimum requirement for developer to use Uitimate effectively:
 - able to review changelog of the entire site
---
+++ packages/docs/app/routes/docs.extra-topics.changelog.tsx
@@ -0,0 +1,15 @@
+import ReactMarkdown from "react-markdown";
+import DocPageLayout from "#/components/internal/layout/DocPageLayout";
+
+const changelogFiles = import.meta.glob('../../CHANGELOG.md', { eager: true, as: 'raw' });
+const changelog = Object.values(changelogFiles)[0];
+
+export default () => {
+  return (
+    <DocPageLayout>
+      <div className="tw:prose tw:dark:prose-invert tw:max-w-none">
+        <ReactMarkdown>{changelog}</ReactMarkdown>
+      </div>
+    </DocPageLayout>
+  )
+}
