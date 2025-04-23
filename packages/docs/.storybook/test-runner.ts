dd1e0394401c634a982895fa1b6ae32b89d885d8
timlee <hungtington.dev@gmail.com>
2025-04-23 08:56:13 -0400

    ✨ feat: able to test certain stories in using Argos

--- packages/docs/.storybook/test-runner.ts
+++ packages/docs/.storybook/test-runner.ts
@@ -2,6 +2,9 @@ import type { TestRunnerConfig } from "@storybook/test-runner";
 import { argosScreenshot } from "@argos-ci/storybook";
 
 const config: TestRunnerConfig = {
+  tags: {
+    include: ['qa'], // Only test stories with 'qa' tag
+  },
   async postVisit(page, context) {
     await argosScreenshot(page, context);
   },
