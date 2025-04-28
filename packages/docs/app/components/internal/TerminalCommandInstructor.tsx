f2c70e406c2c9b84b17b205ddebc0950ae8296a8
timlee <hungtington.dev@gmail.com>
2025-04-28 10:11:30 -0400

    ✨ feat: be able to set up for form component

---
+++ packages/docs/app/components/internal/TerminalCommandInstructor.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Cta } from '../ui/Cta/Cta.tsx';
+import { Icon } from "../ui/Icon/Icon.tsx";
+import { Tabs, TabsList, TabsTrigger } from '../ui/Tabs/Tabs.tsx';
+
+const CLI_TABS = ["pnpm", "yarn", "npm"];
+
+export default function TerminalCommandInstructor({
+  cli,
+  onCopy,
+  copied,
+}: {
+  cli: string,
+  onCopy: (tab: string) => void,
+  copied: boolean,
+}) {
+  const [cliTab, setCliTab] = useState('pnpm');
+
+  function cliForTab(tab: string) {
+    if (!cli) return '';
+    return cli.replace(/^\w+/, tab);
+  }
+
+  return (
+    cli ? (
+      <div className='tw:flex tw:flex-col'>
+        <Tabs value={cliTab} onValueChange={setCliTab} variant="underline">
+          <TabsList>
+            {CLI_TABS.map(tab => (
+              <TabsTrigger key={tab} value={tab}>{tab}</TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+        <Cta muted shapes={['badge']} variant="outline" size="lg" className="tw:cursor-default tw:p-4 tw:font-mono tw:w-full tw:max-w-[450px] tw:justify-between">
+          <span className="tw:py-2 tw:overflow-x-scroll tw:break-all">{cliForTab(cliTab)}</span>
+          <Cta shapes={['icon']} variant="ghost" size="sm" className="tw:shadow-none tw:pointer-events-auto" onClick={() => onCopy(cliForTab(cliTab))}>
+            <Icon icon={copied ? 'lucide:check' : 'lucide:copy'} />
+          </Cta>
+        </Cta>
+      </div>
+    ) : null
+  );
+}
