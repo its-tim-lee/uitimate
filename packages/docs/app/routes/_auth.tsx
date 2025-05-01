9c12c8648f3ec6c6743b2ea2d2c418ad4e2c1492
timlee <hungtington.dev@gmail.com>
2025-05-01 06:57:28 -0400

    ✨ feat: remove things that will cause prod-build fail

--- packages/docs/app/routes/_auth.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router";
-
-export default function Auth() {
-  return <div>
-    <h1>This is the layout of "/auth/*" routes; you can't visit /auth directly,
-      cuz what it does is simply provide a layout without affecting the URL path.</h1>
-    <Outlet />
-  </div>;
-}
--- packages/docs/app/routes/concerts._index.tsx
+++
@@ -1,3 +0,0 @@
-export default function ConcertsIndex() {
-  return <div>Concerts Index; this actually can be done using concerts.tsx, but with _index as the name is for homepage-naming consistency</div>;
-}
