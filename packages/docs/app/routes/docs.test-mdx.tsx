27b25f8e85fed0d1cab56597b42049cfc06608ab
timlee <hungtington.dev@gmail.com>
2025-05-02 17:35:13 -0400

    ✨ feat: just add a testing-only mdx

---
+++ packages/docs/app/routes/docs.test-mdx.tsx
@@ -0,0 +1,17 @@
+import DocPageLayout from "#/components/internal/layout/DocPageLayout";
+import { Mdx } from '#/components/internal/Mdx'
+import { allDocs } from '#/lib/contentlayer'
+
+const doc = allDocs.find(d => {
+  return d.title?.toLowerCase() === 'test-mdx';
+});
+
+export default () => {
+  return (
+    <DocPageLayout>
+      <div className="tw:prose tw:dark:prose-invert tw:max-w-none">
+        <Mdx code={doc?.body?.code || ''} />
+      </div>
+    </DocPageLayout>
+  )
+}
