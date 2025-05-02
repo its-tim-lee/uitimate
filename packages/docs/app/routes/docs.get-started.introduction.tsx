0fd4751ad88927aff52fa984ad60d62edddf8806
timlee <hungtington.dev@gmail.com>
2025-05-02 18:07:16 -0400

    ✨ feat: turn introduction to MDX file

--- packages/docs/app/routes/docs.get-started.introduction.tsx
+++ packages/docs/app/routes/docs.get-started.introduction.tsx
@@ -1,14 +1,16 @@
-import ReactMarkdown from "react-markdown";
 import DocPageLayout from "#/components/internal/layout/DocPageLayout";
+import { Mdx } from '#/components/internal/Mdx'
+import { allDocs } from '#/lib/contentlayer'
 
-const introductionFiles = import.meta.glob('../docs/introduction.md', { eager: true, as: 'raw' });
-const introduction = Object.values(introductionFiles)[0];
+const doc = allDocs.find(d => {
+  return d.title?.toLowerCase() === 'introduction';
+});
 
 export default () => {
   return (
     <DocPageLayout>
       <div className="tw:prose tw:dark:prose-invert tw:max-w-none">
-        <ReactMarkdown>{introduction}</ReactMarkdown>
+        <Mdx code={doc?.body?.code || ''} />
       </div>
     </DocPageLayout>
   )
