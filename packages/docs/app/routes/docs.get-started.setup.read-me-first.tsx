c218ee2730d9edc3a2fcea10d1e86134bff38304
timlee <hungtington.dev@gmail.com>
2025-05-02 13:00:33 -0400

    ✨ feat: remove useless

--- packages/docs/app/routes/docs.get-started.setup.read-me-first.tsx
+++ packages/docs/app/routes/docs.get-started.setup.read-me-first.tsx
@@ -1,10 +1,7 @@
-import ReactMarkdown from "react-markdown";
 import DocPageLayout from "#/components/internal/layout/DocPageLayout";
 import { Mdx } from '#/components/internal/Mdx'
 import { allDocs } from '#/lib/contentlayer'
 
-const readMeFirstFiles = import.meta.glob('../docs/read-me-first.md', { eager: true, as: 'raw' });
-const readMeFirst = Object.values(readMeFirstFiles)[0];
 const doc = allDocs.find(d => {
   return d.title?.toLowerCase() === 'read-me-first';
 });
@@ -13,7 +10,6 @@ export default () => {
   return (
     <DocPageLayout>
       <div className="tw:prose tw:dark:prose-invert tw:max-w-none">
-        <ReactMarkdown>{readMeFirst}</ReactMarkdown>
         <Mdx code={doc?.body?.code || ''} />
       </div>
     </DocPageLayout>
