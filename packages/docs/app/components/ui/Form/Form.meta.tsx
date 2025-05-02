fa58822fa44d18f6e38e25b38da1fdb5408ab697
timlee <hungtington.dev@gmail.com>
2025-05-02 17:34:59 -0400

    ✨ feat: refactor the syntax for remark-code-import in mdx to rehype-mdx-code-props

--- packages/docs/app/components/ui/Form/Form.meta.tsx
+++ packages/docs/app/components/ui/Form/Form.meta.tsx
@@ -1,21 +1,9 @@
-// WARN: Changing any thing from this file,
-// especially creating a new line, will interrupt the reference of `anatomy` below from the documentation
 import type { ComponentMeta } from "#/types/index.ts";
-
+import anatomy from './Form.anatomy.txt?raw';
 export default {
   tags: {
     tutorial: ['WIP']
   },
   description: <span>Build form like a pro 😎 － 100% composible fashion (ie., no render-props) of using <b className='tw:brand'>React Hook Form</b> with <b className='tw:brand'>Zod</b>.</span>,
-  anatomy: `
-    <Form>
-      <FormItem>
-        <FormLabel/>
-        <FormControl/>
-        <FormDescription/>
-        <FormMessage />
-      </FormItem>
-      {/* more <FormItem/> ... */}
-    </Form>
-  `,
+  anatomy
 } as ComponentMeta;
