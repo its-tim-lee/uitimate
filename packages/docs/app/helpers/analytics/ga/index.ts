5a319a5d4239f9d9f51f51792d7456881c51c4a2
timlee <hungtington.dev@gmail.com>
2025-05-02 08:56:15 -0400

    ✨ feat: make GA work without using Firebase

--- packages/docs/app/helpers/analytics/ga/index.ts
+++ packages/docs/app/helpers/analytics/ga/index.ts
@@ -1,13 +1,39 @@
-import { analytics } from '#/helpers/firebase/index.ts';
-import { logEvent } from 'firebase/analytics';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag?: (...args: any[]) => void;
+  }
+}
+
+export const InitializeGA = () => {
+  useEffect(() => {
+    const measurementId = 'G-NG3646V7SM';
+    const script = document.createElement("script");
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+    script.async = true;
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function () {
+        window.dataLayer.push(arguments);
+      };
+      window.gtag('js', new Date());
+      window.gtag('config', measurementId);
+    }
+    document.head.appendChild(script);
+  }, []);
+
+  return null;
+}
 
 export const track = (event: string, params?: Record<string, any>) => {
   if (typeof window === 'undefined') return; // Only run client-side
   if (process.env.NODE_ENV !== 'production') return;
-  if (!analytics) return;
+  if (!window.gtag) return;
   try {
-    logEvent(analytics, event, params);
+    window.gtag('event', event, params);
   } catch (e) {
-    // Optionally handle/log error
+    console.error('Error tracking event:', e);
   }
 }
