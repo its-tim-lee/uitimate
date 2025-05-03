797222744ac36417804c5e28452185af2e3801cc
timlee <hungtington.dev@gmail.com>
2025-05-02 22:28:25 -0400

    ✨ feat: add a root route

--- packages/docs/app/routes.ts
+++ packages/docs/app/routes.ts
@@ -7,9 +7,12 @@ import { flatRoutes } from "@react-router/fs-routes";
  * - Every route in routes.ts is nested inside the special app/root.tsx module (Root Route).
  */
 export default [
+  // HACK: for some reasons, `flatRoutes` can't convert routes/index.tsx into the path `/`,
+  // but `index`, that's why we need to manually convert it at here
+  index("./routes/index.tsx"),
   ...(await flatRoutes({
     rootDirectory: "routes",
-    ignoredRouteFiles: ["routes/**/*.test.{ts,tsx}"],
+    ignoredRouteFiles: ["routes/**/*.test.{ts,tsx}", "routes/index.tsx"],
   }))
 ] satisfies RouteConfig;
 
