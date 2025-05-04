0c5d4e65c6851778010a6101da2a49ba1786ac67
timlee <hungtington.dev@gmail.com>
2025-05-04 10:43:30 -0400

    🐛 fix: site body should be RWD when in wider screen

--- packages/docs/app/components/internal/layout/DocPageLayout.tsx
+++ packages/docs/app/components/internal/layout/DocPageLayout.tsx
@@ -9,21 +9,25 @@ export default function DocPageLayout({ children }: { children: React.ReactNode 
   return (
     <>
       <SiteHeader onSidebarToggle={() => setSidebarOpen((open) => !open)} />
-      <SidebarLayout
-        open={sidebarOpen}
-        onOpenChange={setSidebarOpen}
-        directions={["left", "bottom"]}
-        className="tw:min-h-screen"
-        enableMobileSidebar={true}
-        defaultOpen={true}
-      >
-        <Sidebar className="tw:pt-0 tw:border-r tw:h-svh tw:overflow-y-scroll tw:md:block">
-          <DocsSidebar />
-        </Sidebar>
-        <SidebarPeer className="tw:p-4 tw:h-svh tw:overflow-y-scroll">
-          {children}
-        </SidebarPeer>
-      </SidebarLayout>
+      <div className="container-wrapper">
+        <div className="container">
+          <SidebarLayout
+            open={sidebarOpen}
+            onOpenChange={setSidebarOpen}
+            directions={["left", "bottom"]}
+            className="tw:min-h-screen"
+            enableMobileSidebar={true}
+            defaultOpen={true}
+          >
+            <Sidebar className="tw:pt-0 tw:border-r tw:h-svh tw:overflow-y-scroll tw:md:block">
+              <DocsSidebar />
+            </Sidebar>
+            <SidebarPeer className="tw:p-4 tw:h-svh tw:overflow-y-scroll">
+              {children}
+            </SidebarPeer>
+          </SidebarLayout>
+        </div>
+      </div>
     </>
   );
 }
