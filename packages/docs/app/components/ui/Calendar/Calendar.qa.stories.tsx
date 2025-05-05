d5ac9f31feadfb6ea4094f66be23c4bef8a9a18b
timlee <hungtington.dev@gmail.com>
2025-05-05 14:32:21 -0400

    ✨ feat: write play fn for date-picker

--- packages/docs/app/components/ui/Calendar/Calendar.qa.stories.tsx
+++ packages/docs/app/components/ui/Calendar/Calendar.qa.stories.tsx
@@ -1,14 +1,25 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import CalendarDemo from "#/components/demo/calendar-demo.tsx"
+import CalendarDatePicker from "#/components/demo/recipe/date-picker.tsx"
+import { userEvent, within } from "@storybook/test";
 
 export default {
   title: 'QA/Calendar',
   parameters: {
     layout: 'centered',
   },
-  tags: ['qa'],
+  tags: ['qa', 'fullpage'],
 } as Meta;
 
 export const normal: StoryObj = {
   render: () => <CalendarDemo />,
+};
+
+export const datePicker: StoryObj = {
+  render: () => <CalendarDatePicker />,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const ctaButton = canvas.getByTestId("date-picker-cta");
+    await userEvent.click(ctaButton);
+  }
 };
