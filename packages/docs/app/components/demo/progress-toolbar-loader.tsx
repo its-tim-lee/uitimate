659fcd27679f729d4f0f94601e3674dd68d964ca
timlee <hungtington.dev@gmail.com>
2025-05-05 09:59:26 -0400

    ✨ feat: remove all usages of Flat, and place Flat as Core component

--- packages/docs/app/components/demo/progress-toolbar-loader.tsx
+++ packages/docs/app/components/demo/progress-toolbar-loader.tsx
@@ -1,12 +1,11 @@
-import { Flat } from "../preset/flat"
 import { Progress } from '#/components/ui/Progress/Progress';
 
 // TODO: make this fancy: connect to the real world use case
 export default () => {
   return (
-    <Flat className="tw:w-full tw:h-[50px] tw:relative">
+    <div className="tw:bg-background tw:shadow-sm tw:border tw:border-solid tw:border-zinc-200 tw:rounded-[8px] tw:p-6 tw:w-full tw:h-[50px] tw:relative">
       Toolbar
       <Progress value={60} className="tw:absolute tw:bottom-0 tw:left-0" />
-    </Flat>
+    </div>
   )
 }
--- packages/docs/app/components/internal/InfoBanner.tsx
+++ packages/docs/app/components/internal/InfoBanner.tsx
@@ -1,13 +1,15 @@
 import { type ComponentProps } from "react"
-import { Flat } from "#/components/preset/flat"
 import { Icon } from "#/components/ui/Icon/Icon"
 import { cn } from "#/helpers/utils"
 
-export default ({ children, className }: ComponentProps<'p'>) => {
+export default ({ children, className, ...props }: ComponentProps<'p'>) => {
   return (
-    <Flat size="sm" className={cn("tw:flex tw:items-start tw:gap-2 tw:border-primary not-prose", className)}>
+    <div
+      {...props}
+      className={cn("tw:bg-background tw:shadow-sm tw:border tw:border-solid tw:border-primary tw:rounded-[8px] tw:p-3 tw:flex tw:items-start tw:gap-2 not-prose", className)}
+    >
       <Icon icon='lucide:info' className='tw:mt-1 tw:size-5' />
       <div>{children}</div>
-    </Flat >
+    </div>
   )
 }
