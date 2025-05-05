62cce26f953b0b9c2e1cfb46b51359f0f34b1856
timlee <hungtington.dev@gmail.com>
2025-05-05 08:48:42 -0400

    ✨ feat: modify the component

--- packages/docs/app/components/demo/heading-alert.tsx
+++ packages/docs/app/components/demo/heading-alert.tsx
@@ -8,8 +8,8 @@ import { Icon } from "#/components/ui/Icon/Icon";
 export default () => (
   <div className="tw:flex tw:items-start tw:gap-2 tw:border-primary not-prose tw:w-xl tw:bg-background tw:shadow-sm tw:border tw:border-solid tw:rounded-[8px] tw:p-3">
     <Icon icon='lucide:info' className='tw:mt-1' />
-    <Heading size="h4" className='tw:mb-0'>
-      <HeadingTitle>Updated!</HeadingTitle>
+    <Heading size="h4" className='tw:mb-0 tw:bg-destructive tw:text-destructive-foreground'>
+      <HeadingTitle>You rock!</HeadingTitle>
       <HeadingSubtitle>The application has been updated from v1.2 to v1.3.</HeadingSubtitle>
     </Heading>
   </div>
