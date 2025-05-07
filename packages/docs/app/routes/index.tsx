9f88c15cc14396ee8ea4ad4b65412cbafa15f064
timlee <hungtington.dev@gmail.com>
2025-05-07 08:29:39 -0400

    ✨ feat: temporary add title to the homepage to potentially reduce (not set) issue in GA

--- packages/docs/app/routes/index.tsx
+++ packages/docs/app/routes/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
+export const meta = () => ([{
+  title: "Home of Uitimate",
+}]);
+
 export default function Index() {
   const navigate = useNavigate();
 
