402b6020673c0a9d5750ae6e3665297d4d046360
timlee <hungtington.dev@gmail.com>
2025-05-20 08:28:00 -0400

    🧹 chore: fix the doc of uitimate-starter

--- packages/examples/uitimate-starter/src/App.tsx
+++ packages/examples/uitimate-starter/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="tw:text-slate-600 tw:text-2xl">
       What this boilerplate provides is just setting up "some" configurations from ...,
-      for the others, you need to visit that page to do the rest of the setup.
+      for the others, you need to visit https://ui-timate.web.app/docs/get-started/setup/read-me-first to do the rest of the setup.
     </div>
   )
 }
